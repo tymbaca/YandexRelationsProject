592df12cf06a2ee3dccdc4cb40ddae6cb5f91f30
Tigran Rostomyan <tumb4ka228@gmail.com>
2023-03-17 23:51:53 +0200

    Test data generator

---
+++ tests/generate_imports.py
@@ -0,0 +1,7 @@
+from mimesis import Person
+from mimesis.builtins import RussiaSpecProvider
+from citizen_analyzer.api.pydantic_models import Import, Citizen
+
+
+def create_citizen() -> Citizen:
+    pass
