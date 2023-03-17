225c633dcf36cf9b7def5179d50328f53fb9cfa8
Tigran Rostomyan <tumb4ka228@gmail.com>
2023-03-18 00:15:12 +0200

    Ids hashmap (dict) generation

--- tests/generate_imports.py
+++ tests/generate_imports.py
@@ -3,5 +3,23 @@ from mimesis.builtins import RussiaSpecProvider
 from citizen_analyzer.api.pydantic_models import Import, Citizen
 
 
-def create_citizen() -> Citizen:
+def generate_import(count: int):
+    pass
+
+
+def create_citizen(id, relatives) -> Citizen:
+    citizen = Citizen()
+    pass
+    # citizen_id=
+    # town: str
+    # street: str
+    # building: str
+    # apartment: int
+    # name: str
+    # birth_date: str
+    # gender: str
+    # relatives: list[int])
+
+
+def generate_ids_dict(count, relatives_max) -> dict[int, set[int]]:
     pass
