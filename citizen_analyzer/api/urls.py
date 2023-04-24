c1d06d5804254411641d120a4e3fda418f23e683
Tigran Rostomyan <tumb4ka228@gmail.com>
2023-04-24 15:25:06 +0200

    Minor changes

--- citizen_analyzer/api/urls.py
+++ citizen_analyzer/api/urls.py
@@ -1,8 +1,7 @@
 from aiohttp import web
-from controllers import load_and_return_json, import_citizens
+from controllers import import_citizens
 
 
 def setup_routes(app: web.Application):
-    app.router.add_get('/', load_and_return_json)
     app.router.add_post('/imports', import_citizens)
     # app.router.add_patch('imports/{import_id}/citizens/{citizen_id}')
