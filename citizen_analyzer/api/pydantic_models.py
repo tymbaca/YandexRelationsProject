acb813bb220e47e57023397b5dea1546348afae1
Tigran Rostomyan <tumb4ka228@gmail.com>
2023-04-24 14:50:52 +0200

    Added relatives validation

--- citizen_analyzer/api/pydantic_models.py
+++ citizen_analyzer/api/pydantic_models.py
@@ -31,7 +31,23 @@ class ImportModel(BaseModel):
         import_dict: dict = self.dict()
         import_json = json.dumps(import_dict, ensure_ascii=False)
         return import_json
-
-
+    
+    @validator("citizens")
+    def validate_relatives(cls, citizens):
+        for citizen in citizens:
+            for relative_id in citizen.relatives:
+                relative_matches: list[CitizenModel] = [citizen for citizen in citizens if citizen.id == relative_id]
+    
+                if len(relative_matches) == 1 and citizen.id in relative_matches[0].relatives:
+                    continue
+                elif len(relative_matches) == 1 and citizen.id not in relative_matches[0].relatives: 
+                    raise ValueError(f"Not mutual relation: Citizen {citizen.id} -> Citizen {relative_id}")
+                elif len(relative_matches) == 0:
+                    raise ValueError(f"Citizen with {citizen.id} ID have a relative with {relative_id} ID but there are no citizen with that ID")
+                else:
+                    raise ValueError(f"More multiple citizens with same ID ({relative_id})")
+        return citizens
+                    
+                
 if __name__ == '__main__':
     pass
