d9fe4cc918109c20fd50707ca352655c8758487e
Tigran Rostomyan <tumb4ka228@gmail.com>
2023-04-24 15:42:14 +0200

    Deleted commented code

--- tests/api/test_controllers.py
+++ tests/api/test_controllers.py
@@ -12,19 +12,6 @@ import_generator = ImportGenerator()
 ig = import_generator
 
 
-# @pytest.mark.parametrize("request_body", [(ig.generate_import(1).json()), (ig.generate_import(1).json()),])
-# def test_imports(request_body: requests.Request):
-#     response = requests.post(url=IMPORTS_ENDPOINT, data=request_body)
-#     success_response_data = {"data": {"import_id": 1}}
-#     assert response.status_code == 201
-#     assert response.json() == success_response_data
-#
-#     response = requests.post(url=IMPORTS_ENDPOINT, data=request_body)
-#     success_response_data = {"data": {"import_id": 2}}
-#     assert response.status_code == 201
-#     assert response.json() == success_response_data
-
-
 @pytest.mark.parametrize("request_body", incorrect_relatives)
 def fail_imports(request_body: str):
     response = requests.post(url=IMPORTS_ENDPOINT, data=request_body)
