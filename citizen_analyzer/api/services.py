7015247ae66d338dfaee874772c30a5527c6ebf7
Tigran Rostomyan <tumb4ka228@gmail.com>
2023-04-24 15:26:24 +0200

    `handle_import_and_return_id` function
    Will do database stuff and return id to be insert in response json

---
+++ citizen_analyzer/api/services.py
@@ -0,0 +1,26 @@
+from sqlalchemy.orm import Session
+
+from citizen_analyzer.api import pydantic_models
+from citizen_analyzer.db import models
+from citizen_analyzer.app import app
+
+def handle_import_and_return_id(import_model: pydantic_models.ImportModel) -> None:
+    with Session(app.database_engine) as session:
+        db_import = models.Import()
+        session.add(db_import)
+        
+        for citizen in import_model.citizens:
+            db_citizen = models.Citizen(id=citizen.id,
+                                        import_id=db_import.id,
+                                        town=citizen.town,
+                                        street=citizen.street,
+                                        building=citizen.building,
+                                        apartment=citizen.apartment,
+                                        name=citizen.name,
+                                        birthdate=citizen.birthdate,
+                                        gender=citizen.gender)
+            session.add(db_citizen)
+        session.commit()
+        return db_import.id
+
+
