0e828a1df548776e8d1425a9247b7ec754334755
Tigran Rostomyan <tumb4ka228@gmail.com>
2023-04-24 15:37:26 +0200

    Minor refactoring

--- tests/api/test_controllers.py
+++ tests/api/test_controllers.py
@@ -2,27 +2,30 @@ import pytest
 import requests
 
 from tests.generate_imports import ImportGenerator
+from tests.api.values import incorrect_relatives
 from citizen_analyzer.api import controllers
 
 ENDPOINT = "http://0.0.0.0:8080/"
-
+IMPORTS_ENDPOINT = ENDPOINT + "imports"
 
 import_generator = ImportGenerator()
 ig = import_generator
 
 
-@pytest.mark.parametrize("request_body", [(ig.generate_import(1).json(), ig.generate_import(1).json()),
-                                          ])
-def test_imports(request_body: requests.Request):
-    endpoint = ENDPOINT + "imports"
-    response = requests.post(url=endpoint, data=request_body)
-    success_response_data = {"data": {"import_id": 1}}
-    assert response.status_code == 201
-    assert response.json() == success_response_data
-
-    response = requests.post(url=endpoint, data=request_body)
-    success_response_data = {"data": {"import_id": 2}}
-    assert response.status_code == 201
-    assert response.json() == success_response_data
+# @pytest.mark.parametrize("request_body", [(ig.generate_import(1).json()), (ig.generate_import(1).json()),])
+# def test_imports(request_body: requests.Request):
+#     response = requests.post(url=IMPORTS_ENDPOINT, data=request_body)
+#     success_response_data = {"data": {"import_id": 1}}
+#     assert response.status_code == 201
+#     assert response.json() == success_response_data
+#
+#     response = requests.post(url=IMPORTS_ENDPOINT, data=request_body)
+#     success_response_data = {"data": {"import_id": 2}}
+#     assert response.status_code == 201
+#     assert response.json() == success_response_data
 
 
+@pytest.mark.parametrize("request_body", incorrect_relatives)
+def fail_imports(request_body: str):
+    response = requests.post(url=IMPORTS_ENDPOINT, data=request_body)
+    assert response.status_code == 400
